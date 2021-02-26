2827517de6c4a628aa405f21c06c46573086b7a4
adrianpura <pura.adriano@gmail.com>
2021-02-27 02:56:36 +0800

    Signed-off-by: adrianpura <pura.adriano@gmail.com>

---
+++ resources/views/_publish_tweet-panel.blade.php
@@ -0,0 +1,28 @@
+<div class="border border-blue-400 rounded-lg px-8 py-6 mb-8">
+    <form method="">
+        <textarea
+            name="body"
+            class="w-full"
+            placeholder="What's up doc?"
+            ></textarea>
+
+        <hr class="my-4">
+
+        <footer class="flex justify-between items-center">
+            <img
+                src="https://i.pravatar.cc/40"
+                alt="your avatar"
+                class="rounded-full mr-2"
+                width="50"
+                height="50"
+                >
+
+            <button
+                type="submit"
+                class="bg-blue-500 hover:bg-blue-600 rounded-lg shadow px-10 text-sm text-white h-10"
+                >
+                Publish
+            </button>
+        </footer>
+    </form>
+</div>
